26c0f8bd70483b6f7105b12b483958029f3f209a
zeib01 <ibragimovazeinep@gmail.com>
2024-02-22 17:31:58 +0100

    structure

---
+++ js/task-1.js
@@ -0,0 +1,6 @@
+const pricePerDroid = 800;
+const orderedQuantity = 6;
+const deliveryFee = 50;
+const totalPrice = pricePerDroid * orderedQuantity + deliveryFee;
+const message = `"You ordered droids worth ${totalPrice} credits"`;
+console.log("🚀 ~ message:", message)
